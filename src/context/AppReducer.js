4e27fcb5438ecb28cafc1dcdf74172f7b7703fd4
yakup <lvashy86@gmail.com>
2024-04-20 15:40:26 +0300

    update

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const appReducer = (state, action) => {
   switch (action.type) {
     case "ADD_MOVIE_TO_WATCHLIST":
       return {
@@ -37,3 +37,5 @@ export default (state, action) => {
       return state;
   }
 };
+
+export default appReducer;
